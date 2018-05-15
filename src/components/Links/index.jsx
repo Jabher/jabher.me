102d94a70e08ebddc41c4725b094b39f13719119
jabher <vsevolod.rodionov@gmail.com>
2018-05-16 01:50:30 +0300

    remove socials

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -9,8 +9,6 @@ class Links extends React.Component {
       telegram: author.telegram,
       twitter: author.twitter,
       github: author.github,
-      vk: author.vk,
-      rss: author.rss,
       email: author.email
     };
 
--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -60,8 +60,6 @@ export const pageQuery = graphql`
           telegram
           twitter
           github
-          rss
-          vk
         }
       }
     }
